ffd4ad7c5efa31a8b57c4e9f312fd2c0815f70e7
Asaf Artsi <asafa@chargeflow.io>
2025-12-10 16:57:54 +0200

    Added Zod, Validation to the Prompt and error handling

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import z from 'zod';
 
 dotenv.config();
 
@@ -14,26 +15,47 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-   res.send('Hello from server!');
+   res.send(`hello world!`);
 });
 
 app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello from Server' });
+   res.json({ message: 'Hello World!' });
+});
+
+const conversations = new Map<string, string>();
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required.')
+      .max(1000, 'Prompt is too long (max 1000 characters)'),
+   conversationId: z.uuid(),
 });
 
 app.post('/api/chat', async (req: Request, res: Response) => {
-   const { prompt } = req.body;
+   const parseResult = chatSchema.safeParse(req.body);
+   if (!parseResult.success) {
+      res.status(400).json(z.treeifyError(parseResult.error));
+      return;
+   }
 
-   const response = await client.responses.create({
-      model: 'gpt-4o-mini',
-      input: `what is the capital of ${prompt}, answer in one word.`,
-      temperature: 0.2,
-      max_output_tokens: 100,
-   });
-
-   res.json({ message: response.output_text });
+   try {
+      const { prompt, conversationId } = req.body;
+      const response = await client.responses.create({
+         model: 'gpt-4o-mini',
+         input: prompt,
+         temperature: 0.2,
+         max_output_tokens: 100,
+         previous_response_id: conversations.get(conversationId),
+      });
+      conversations.set(conversationId, response.id);
+      res.json({ message: response.output_text });
+   } catch (error) {
+      res.status(500).json({ error: ' Failed to generate a response.' });
+   }
 });
 
 app.listen(PORT, () => {
-   console.log(`Server is running on http://localhost:${PORT}`);
+   console.log(`Server is runnig on http://localhost:${PORT}`);
 });
